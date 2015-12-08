a73309d93dea16f44ff170f5742507ec424a3554
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2015-12-07 17:23:08 -0800

    Day 6 part 1

---
+++ python/06.py
@@ -0,0 +1,56 @@
+import re
+
+filename = "06.txt"
+
+regex = re.compile(r'^(turn on|toggle|turn off)\s+(\d+,\d+)\s.*\s(\d+,\d+)$')
+
+lights = [[False for x in range(1000)] for x in range(1000)]
+instructions = [x.strip() for x in open(filename).readlines()]
+
+def turn_on(lights, start, finish):
+    (start_x, start_y) =map(int, start.split(","))
+    (finish_x, finish_y) = map(int, finish.split(","))
+
+    for x in range(start_x, finish_x + 1):
+        for y in range(start_y, finish_y + 1):
+            lights[x][y] = True
+
+def turn_off(lights, start, finish):
+    (start_x, start_y) =map(int, start.split(","))
+    (finish_x, finish_y) = map(int, finish.split(","))
+
+    for x in range(start_x, finish_x + 1):
+        for y in range(start_y, finish_y + 1):
+            lights[x][y] = False
+
+def toggle(lights, start, finish):
+    (start_x, start_y) =map(int, start.split(","))
+    (finish_x, finish_y) = map(int, finish.split(","))
+
+    for x in range(start_x, finish_x + 1):
+        for y in range(start_y, finish_y + 1):
+            lights[x][y] = not lights[x][y]
+
+
+for i in instructions:
+    m = regex.match(i)
+    if not m:
+        break
+
+    (instruction, start, finish) = m.groups()
+
+    if instruction.strip() == "turn on":
+        turn_on(lights, start, finish)
+    elif instruction.strip() == "toggle":
+        toggle(lights, start, finish)
+    elif instruction.strip() == "turn off":
+        turn_off(lights, start, finish)
+
+lit = 0
+
+for x in range(1000):
+    for y in range(1000):
+        if lights[x][y]:
+            lit = lit + 1
+
+print lit
