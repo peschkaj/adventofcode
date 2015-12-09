af6cb4b6a34c0e5f72fa40edb0729dda69b92a74
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2015-12-08 20:39:57 -0800

    Adding the solution to the second part of Day 1

--- javascript/01.js
+++ javascript/01.js
@@ -4,17 +4,35 @@ var filename = '01.txt';
 var contents = fs.readFileSync(filename).toString().trim();
 
 function countFloors(floors) {
-  var symbols = floors.split('');
-  console.log(symbols);
-  var floorCount = symbols.map(s => {
-                                      switch(s) {
-                                        case '(': return 1;
-                                        case ')': return -1;
-                                        default: return;
-                                      }
-                                    })
-                                    .reduce((previous, current) => previous + current);
-  return floorCount;
+  return floors.split('')
+               .map(s => {
+                            switch(s) {
+                              case '(': return 1;
+                              case ')': return -1;
+                              default: return;
+                            }
+                          })
+                          .reduce((previous, current) => previous + current);
+}
+
+function getPositionAtFloor(floors, floor) {
+  var found = false;
+  var position = 1;
+  var floor = 0;
+
+  floors.split('').forEach(f => {
+    floor += (f === '(') ? 1 : -1;
+    if (floor === -1) {
+      found = true;
+    }
+
+    if (!found) {
+      position++;
+    }
+  });
+
+  return position;
 }
 
 console.log(countFloors(contents));
+console.log(getPositionAtFloor(contents, -1));
